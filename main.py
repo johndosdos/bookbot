f629a7fb979538fb5eb5d326f0ebfff9f3e84823
John <128350391+alpaOo@users.noreply.github.com>
2024-03-07 04:01:22 +0800

    Implement basic input validation for .txt file extension

--- main.py
+++ main.py
@@ -34,20 +34,31 @@ def get_book_word_count(book: str) -> int:
     return len(string_list)
 
 
-def open_book() -> str:
-    with open("books/frankenstein.txt") as file:
+def open_book(file_path: str) -> str:
+    with open(file_path) as file:
         file_contents = file.read()
 
         return file_contents
 
 
 def main():
-    book = open_book()
-    word_count = get_book_word_count(book)
-    letter_freq_dict = get_book_letter_freq(book)
-    sorted_freq_dict = sort_freq_dict(letter_freq_dict)
+    try:
+        print("\n")
+        file_path = input("Please enter the file path of your desired book: ")
 
-    print_report(word_count, sorted_freq_dict)
+        match file_path:
+            case file_path if file_path.endswith(".txt"):
+                book = open_book(file_path)
+            case _:
+                raise Exception("[INPUT ERROR] Please enter a .txt file\n")
+
+        word_count = get_book_word_count(book)
+        letter_freq_dict = get_book_letter_freq(book)
+        sorted_freq_dict = sort_freq_dict(letter_freq_dict)
+
+        print_report(word_count, sorted_freq_dict)
+    except Exception as err:
+        print(err)
 
 
 main()
